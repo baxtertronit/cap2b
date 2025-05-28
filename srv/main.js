34e06a678c41c62b7b9f3c228fc3d3a6f5d67a9c
branimir.kostic@tron-it.com <branimir.kostic@tron-it.com>
2025-05-28 09:00:04 +0000

    lesson 13

--- srv/main.js
+++ srv/main.js
@@ -4,7 +4,11 @@ const cds = require('@sap/cds')
 
 module.exports = cds.service.impl(function () {
     console.log('I am in the anonymous function...')
-    this.on('READ', 'Books', () => {
-        console.log("Handling READ of Books")
+    this.on('READ', 'Books', (req, ...things) => {
+        return [{
+            ID: cds.utils.uuid(), 
+            title: "The title", 
+            author_ID: 2
+        }];
     })
 })
